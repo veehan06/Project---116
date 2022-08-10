df181829bebc432aec7bcb5d004c2f88ac15c845
veehan06 <86970245+veehan06@users.noreply.github.com>
2022-08-10 07:13:23 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 noseX = 0;
 noseY = 0;
+Xcoord = 0;
+Ycoord = 0
 function preload(){
     m = loadImage("https://i.postimg.cc/3x3QzSGq/m.png");
+    li = loadImage("https://i.postimg.cc/PxFvYgkv/l1.png")
 }
 function setup(){
     canvas = createCanvas(300,300);
@@ -15,6 +18,7 @@ function setup(){
 function draw(){
     image(video,0,0,300,300);
     image(m, noseX, noseY, 30, 30)
+    image(li, Xcoord, Ycoord, 30, 30)
 }
 function take_snap(){
     save("FilteredImage");
@@ -28,6 +32,8 @@ function gotPoses(results){
         console.log("nose X = " + results[0].pose.nose.x);
         console.log("nose Y = " + results[0].pose.nose.y);
         noseX = results[0].pose.nose.x - 15;
-        noseY = results[0].pose.nose.y;
+        noseY = results[0].pose.nose.y - 3;
+        Xcoord = results[0].pose.nose.x - 15;
+        Ycoord = results[0].pose.nose.y + 15;
     }
 }
